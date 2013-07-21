c03ad9554615b98889cf564bcd9e8d1547061e88
Vitalii Nagara <duke@ejoom.com>
2013-07-21 10:46:45 +0300

    Remove view_manager key from config to avoid twig module error.

--- config/module.config.php
+++ config/module.config.php
@@ -33,12 +33,6 @@ return array(
         ),
     ),
 
-    'view_manager' => array(
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-
     //defaults
     'libra_locale' => array(
         //default locale, not alias
